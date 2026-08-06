0915075c231584d05f1e206a79ab8f78a3c8b5df
shubhamxpawar <realitymaniac7@gmail.com>
2026-08-07 03:53:34 +0530

    project done - count command added

--- 32-runtimes/word-counter/src/commands/count/index.ts
+++ 32-runtimes/word-counter/src/commands/count/index.ts
@@ -1,13 +1,42 @@
 import { Command } from "@oclif/core";
+import { Args } from "@oclif/core";
+import { readFile } from "fs/promises";
 
 export class Count extends Command {
-    static args = {}
+    static args = {
+        filePath: Args.file({
+            description: 'Path to the input file',
+            required: true, 
+            exists: true,   
+        }),
+    }
     static description = 'count the characters'
     static examples = []
     static flags = {}
 
     async run(): Promise<void> {
-        await this.parse(Count)
-        this.log('This is the count : ')
+        try {
+            const {args} = await this.parse(Count)
+            this.log(`${args.filePath} : `)
+
+            const {wc, cc} = await this.countWordsAndChar(args.filePath)
+
+            this.log(`total number of words are : ${wc}`)
+            this.log(`total number of characters are : ${cc}`)
+        } catch (error) {
+            this.error("couldnt parse this file ")
+        }
+    }
+
+    async countWordsAndChar(file) {
+        const text : string = await readFile(file, {encoding : 'utf-8'})
+
+        const words = text.trim().split(/\s+/)
+
+        const wc = words.length
+        const cc = text.length
+
+        // console.log(wc, cc)
+        return {wc, cc}
     }
 }
